be3a5ff6ad1a62fa2527c09d7fcca3529bbd7b1d
WeiRenSyong <wesy2487@colorado.edu>
2026-05-27 15:05:28 -0600

    Update UserFile_Analyze_Wei_Ren.py

--- UserFile_Analyze_Wei_Ren.py
+++ UserFile_Analyze_Wei_Ren.py
@@ -19,7 +19,7 @@ print("helper_misc  ->", hm.__file__)
 
 # %%  Get sample name and device information
 # Define data directory
-data_dir = Path(r"C:\Users\user\Documents\GitHub\Measurements\Cooldown_77_Line4-MQC_Ta_Qc2M_01 (TBD)")
+data_dir = Path(r"C:\Users\user\Documents\GitHub\Measurements\Cooldown_77_Line2-MQC_Nb_Qc1M_01 (TBD)")
 
 # Safety check
 if not data_dir.exists():
